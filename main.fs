ce5a435646542e125acf2a5cfcc1d50db06961f0
wa1tnr <wa1tnr@users.noreply.github.com>
2021-12-27 14:09:26 +0000

    proto gpio stuff part aa

--- main.fs
+++ main.fs
@@ -5,10 +5,13 @@
 target
 turnkey
     decimal
+    initGPIO
 
 : test  ." this is going to be good 27 Dec 2021 at 13:29:55 UTC" ;
 
-: kurtz 43 #, emit 32 #, emit 43 #, emit cr 1 #, 2 #, 3 #, -99 #, -98 #, -97 #, .s ;
+: kurtz cr 1 #, 2 #, 3 #, -99 #, -98 #, -97 #, .s ;
+
+: togpin 13 #, ;
 
 : said
   43 #, emit
